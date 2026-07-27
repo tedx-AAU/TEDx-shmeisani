87c6430ab7c1064963b6b61f61bc45109c868f88
Malek-Alian <malekalian2002@gmail.com>
2026-07-27 20:28:56 +0300

    Fix Email Service

--- server/config/email.js
+++ server/config/email.js
@@ -4,8 +4,8 @@ const nodemailer = require('nodemailer');
 const createTransporter = () => {
   return nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
       user: 'tedxalshmaisani.jo@gmail.com',
       pass: 'vvtoqykmsxsxyhme',
